29a57d4720022db44819f1c8ecf4065fc2e879c2
Ermit3 <erwan.ngma@gmail.com>
2024-01-05 14:28:23 +0100

    Correction: minor bug

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,7 +4,7 @@ import { SceneInterface } from "@/types";
 
 async function getData() {
   try {
-    const response = await fetch("https://127.0.0.1:3000/api/scenes", {
+    const response = await fetch("http://127.0.0.1:3000/api/scenes", {
       // No cache
       next: { revalidate: 0 },
     });
