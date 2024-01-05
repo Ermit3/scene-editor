28c5e8516d8361412fea96c72d7846326b977d46
Ermit3 <erwan.ngma@gmail.com>
2024-01-05 13:51:54 +0100

    Correction: minor bug

--- src/hooks/useSetMesh.tsx
+++ src/hooks/useSetMesh.tsx
@@ -1,6 +1,6 @@
 import { setMesh } from "@/db";
 
-async function useSetMesh(type: string, sceneId: string) {
+async function useSetMesh(type: "cube" | "plane" | "sphere", sceneId: string) {
   const newMesh = await setMesh({ type, sceneId });
   return newMesh;
 }
